ae2fe8e09d4df6d761191af15f8f5609049f8820
stasulam <l.ambroziak@protonmail.com>
2023-10-21 14:06:46 +0200

    add: output file with name based on date.

--- hadoop/map-reduce/run.sh
+++ hadoop/map-reduce/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
+dt=`date +%Y%m%d-%H%M%S`
+
 hadoop-streaming \
+    -D mapreduce.jobs.reduces=4 \
     -input /tmp/pw312/people-data.txt \
-    -output /tmp/pw312/output/ \
+    -output /tmp/pw312/output/$dt/ \
     -mapper ./mapper.py \
     -reducer ./reducer.py \
     -file ./reducer.py \
